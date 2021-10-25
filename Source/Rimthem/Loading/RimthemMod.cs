80302cf1ad8613f8c45ffa0b6dc6ec24c86e5912
Maxim <MaximSw@web.de>
2021-10-25 12:39:31 +0200

    Project Setup
    Created basic C# project

---
+++ Source/Rimthem/Loading/RimthemMod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Rimthem
+{
+    public class RimthemMod : Mod
+    {
+        public RimthemMod(ModContentPack content) : base(content)
+        {
+        }
+    }
+}
---
+++ Source/Rimthem/Patches/RenderPatches.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Verse;
+
+namespace Rimthem
+{
+    public static class RenderPatches
+    {
+        [HarmonyPatch(typeof(PawnRenderer), nameof(PawnRenderer.DrawBodyApparel))]
+        public static class DrawBodyApparel_Patch
+        {
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var instructionList = instructions.ToList();
+                for (var i = 0; i < instructionList.Count; i++)
+                {
+                    var code = instructionList[i];
+                    yield return code;
+                }
+            }
+        }
+    }
+}
